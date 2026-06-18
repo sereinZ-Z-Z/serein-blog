a10fd3bde08dc4e263b4374d3fe4732aa44d6822
Serein <33735@users.noreply.github.com>
2026-06-18 20:49:45 +0800

    Update blog start date

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,7 +7,7 @@ export const siteConfig: SiteConfig = {
 	title: "Serein",
 	subtitle: "记录生活，分享知识，探索未来。",
 	siteURL: "https://sereinz.top/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2024-09-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-06-18", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
 
